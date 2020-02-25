ec50811db533e129caff478bf24f8a3b0084c052
Javier Repetto de los Dolores <javierrepetto@MacBook-Pro-de-Javier.local>
2020-02-25 09:36:23 +0100

    estructura de carpetas

---
+++ starter-code/index.js
@@ -0,0 +1,13 @@
+const factsArray = ["70% of your cat's life is spent asleep.", "The cat has 500 skeletal muscles (humans have 650)."]
+
+let url = 'https://catfact.ninja/fact/' // Visit https://catfact.ninja/ to read the documentation. 
+
+// ITERACIÓN 1: Añade las dos curiosidades de la array para que se despliegen en la lista del HTML.
+
+// ITERACIÓN 2: Cada vez que el usuario pulse el botón de Remove a cat fact, elimina el último elemento de la lista. 
+
+// ITERACIÓN 3: Cada vez que el usuario pulse el botón de añadir una curiosidad, llama a la API y añade una curiosidad Random. 
+// ITERACIÓN 3.BONUS: Asegurate de que nunca se despliegen curiosidades repetidas.
+
+// ITERACIÓN 4: Añade un elemento input al HTML de tipo number. Cuando el usuario cambie el número de este input cambia el texto de los botones para que aparezca "Add / Remove x cat facts"
+// ITERACIÓN 4.1: modifica las funciónes anteriores para que al hacer click en el botón se añadan o quiten este número de curiosidades. 
